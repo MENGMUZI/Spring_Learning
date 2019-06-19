31a5c1201c89e5175001a45c558dd40086688b3d
孟木子 <18351923886@163.com>
2019-06-19 20:23:12 +0800

    spring_2 spring_AOP的后置通知

--- spring_2/src/com/mmz/spring/aop/impl/LoggingAspect.java
+++ spring_2/src/com/mmz/spring/aop/impl/LoggingAspect.java
@@ -1,6 +1,7 @@
 package com.mmz.spring.aop.impl;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -31,7 +32,18 @@ public class LoggingAspect {
     public void beforeMethod(JoinPoint joinPoint){
         String methodName = joinPoint.getSignature().getName();
         List<Object> args = Arrays.asList(joinPoint.getArgs());
-
         System.out.println("The method: " + methodName + " begins with: " + args);
     }
+
+
+
+    //后置通知：在目标方法执行后(无论是否发生异常)，执行通知
+    //在后置通知中还不能访问目标方法执行的结果
+    @After("execution(* com.mmz.spring.aop.impl.*.*(int,int))")
+    public void afterMethod(JoinPoint joinPoint){
+        String methodName = joinPoint.getSignature().getName();
+        System.out.println("The method: " + methodName + " ends");
+    }
+
+
 }
