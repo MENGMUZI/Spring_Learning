1f6122c853865c8aecbd8692992ba0fb15e82ce4
孟木子 <18351923886@163.com>
2019-06-20 20:07:08 +0800

    spring_4 spring的声明式事务

--- spring_4/src/com/mmz/spring/tx/BookShopServiceImpl.java
+++ spring_4/src/com/mmz/spring/tx/BookShopServiceImpl.java
@@ -2,6 +2,7 @@ package com.mmz.spring.tx;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author : mengmuzi
@@ -15,7 +16,8 @@ public class BookShopServiceImpl implements BookShopService{
     @Autowired
     private BookShopDao bookShopDao;
 
-
+    //添加事务注解
+    @Transactional
     @Override
     public void purchase(String username, int bid) {
 
